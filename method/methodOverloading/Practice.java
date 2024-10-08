b0c487f304d3d839251d7e58ac1a74fc1bfe1524
Satish Yadav <154046509+satishyadav18@users.noreply.github.com>
2024-10-08 12:20:04 +0530

    added

--- method/methodOverloading/Practice.java
+++ method/methodOverloading/Practice.java
@@ -1,9 +1,14 @@
 public class Practice{
 
+  
+    static byte max(byte x, byte y){
+        return x > y ? x : y;
+    }
+  
     static int max(int x, int y){
         return x > y ? x : y;
     }
-
+                                //single float type method can be used for both int and float
     static float max(float x, float y){
         if(x > y){
             return x;
@@ -14,8 +19,13 @@ public class Practice{
     }
 
     public static void main(String[] args){
+
+        byte a=19, b=24;
+
+        System.out.println(max(a,b));
+
         System.out.println(max(10, 5));
 
-         System.out.println(max(5.25f, 7.8f));
+        System.out.println(max(5.25f, 7.8f));
     }
 }
