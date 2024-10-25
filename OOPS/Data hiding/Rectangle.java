450181ffe290416f4421c4f6ee4e506034a503dd
Satish Yadav <154046509+satishyadav18@users.noreply.github.com>
2024-10-25 21:49:53 +0530

    Added

--- OOPS/Data hiding/Rectangle.java
+++ OOPS/Data hiding/Rectangle.java
@@ -1,8 +1,24 @@
 import java.util.*;
 
 class Rect{
-    public float length;
-    public float breadth;
+    private float length;
+    private float breadth;
+
+    public float getLength(){
+        return length;
+     }
+    
+    public float getBreaath(){
+        return breadth;
+    }
+    
+    public void setLength(float l){
+        length = l;
+    }
+
+    public void setBreadth(float b){
+        breadth = b;
+    }
 
     public float area(){
         return length * breadth;
@@ -26,14 +42,16 @@ class Rectangle{
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length: ");
-        r.length = sc.nextFloat();
+        r.setLength(sc.nextFloat()); 
         System.out.print("Enter the breadth: ");
-        r.breadth = sc.nextFloat();
+        r.setBreadth(sc.nextFloat());
 
         float a = r.area();
         float p = r.perimeter();
         boolean b = r.isSquare();
 
+        System.out.println("Length: "+r.getLength());
+        System.out.println("Breadth: "+r.getBreaath());
         System.out.println("Area is: "+a);
         System.out.println("Perimeter is: "+p);
         System.out.println("Is a Square: "+b);
