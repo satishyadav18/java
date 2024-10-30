9fcbf87a96b6a7b2f82eb61ea3b161f9a70eab1a
Satish Yadav <154046509+satishyadav18@users.noreply.github.com>
2024-10-30 23:45:34 +0530

    Added

---
+++ OOPS/DataHidingAndConstructor/Rect2.java
@@ -0,0 +1,58 @@
+class RectangleTest{
+    private double length; 
+    private double breadth;
+
+    public RectangleTest(){
+        length = 1;
+        breadth = 1;
+    }
+
+    public RectangleTest(double l, double b){
+        setLength(l);
+        setBreadth(b);
+    }
+
+    public RectangleTest(double s){
+        length = breadth = s;
+    }
+
+    public double getLength(){
+        return length;
+     }
+    
+    public double getBreaath(){
+        return breadth;
+    }
+    
+    public void setLength(double l){
+        length = l;
+    }
+
+    public void setBreadth(double b){
+        breadth = b;
+    }
+
+    public double area(){
+        return getLength() * getBreaath();
+    }
+
+    public double perimeter(){
+        return 2 * (getLength() + getBreaath());
+    }
+
+    boolean isSquare(){
+        if(length == breadth){
+            return true;
+        }
+        return false;
+    }
+}
+
+public class Rect2{
+    public static void main(String[] args){
+        RectangleTest r = new RectangleTest(-7, -5);
+        System.out.println("Area is: "+r.area());
+        System.out.println("Perimeteris: "+r.perimeter());
+        System.out.println("Is Square: "+r.isSquare());
+    }
+}
