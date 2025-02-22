cded6d79de87435268218aa6ddbc2df537d35bec
Satish Yadav <yadavsatish212004@gmail.com>
2025-02-22 20:02:06 +0530

    added

---
+++ Inheritance/ThisSuper.java
@@ -0,0 +1,20 @@
+class Rectangle{
+    
+    int length;
+    int breadth;
+
+    public Rectangle(int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    System.out.println({"Lenght of rectangle is: "+this.length});
+    System.out.println({"Lenght of rectangle is: "+this.breadth});
+
+}
+
+public class ThisSuper{
+    public static void main(String[] args){
+        
+    }
+}
