48cb2ec8913b44f26748f0bf03d7ec8e755946c1
Satish Yadav <yadavsatish212004@gmail.com>
2025-02-27 20:41:16 +0530

    added

--- Inheritance/ThisSuper.java
+++ Inheritance/ThisSuper.java
@@ -2,6 +2,7 @@ class Rectangle{
     
     int length;
     int breadth;
+    int x = 10;
 
     public Rectangle(int length, int breadth){
         this.length = length;
@@ -15,11 +16,34 @@ class Rectangle{
 
 }
 
+class Cuboid extends Rectangle{
+    int height;
+    int x = 20;
+
+    public Cuboid(int l, int b, int h){
+        super(l,b);
+        height = h;
+    }
+
+    void display(){
+        System.out.println("Super x: "+super.x);
+        System.out.println("Child x: "+x);
+        System.out.println("Length of cuboid: "+length);
+        System.out.println("Breadth of cuboid: "+breadth);
+        System.out.println("Height of cubid: "+height);
+        
+    }
+}
+
+
 public class ThisSuper{
     public static void main(String[] args){
         Rectangle r1 = new Rectangle(5,4);
         Rectangle r2 = new Rectangle(8,3);
         r1.display();
         r2.display();
+
+        Cuboid c = new Cuboid(12,9,20);
+        c.display();
     }
 }
