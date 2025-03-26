d586a9d4d0f1a60507af4c3611eba6ccd5233dc6
Satish Yadav <yadavsatish212004@gmail.com>
2025-03-27 01:46:57 +0530

    added

---
+++ Inheritance/Method Overriding/OverridingTV.java
@@ -0,0 +1,43 @@
+class TV{
+    public void switchOn(){
+        System.out.println("TV is switched on");
+    }
+    public void changeChannel(){
+        System.out.println("Channel is changed");
+    }
+}
+
+class SmartTV extends TV{
+    @Override
+    public void switchOn(){
+        System.out.println("Smart TV is switched on");
+    }
+
+    @Override
+    public void changeChannel(){
+        System.out.println("Channel is changed in Smart TV");
+    }
+
+    public void browse(){
+        System.out.println("Smart TV Browsing");
+    }
+}
+
+
+public class OverridingTV{
+    public static void main(String[] args){
+        // TV tv = new TV();
+        // tv.switchOn();
+        // tv.changeChannel(); 
+
+        SmartTV stv = new SmartTV();
+        stv.switchOn();
+        stv.changeChannel();
+        stv.browse();
+
+        TV tv = new SmartTV();
+        tv.switchOn();
+        tv.changeChannel();
+        //gittv.browse(); //Error  
+    }
+}
