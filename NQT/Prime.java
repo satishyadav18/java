67895a285562de52f4e533ffc0dc6d7bda09c4fc
Satish Yadav <yadavsatish212004@gmail.com>
2026-02-11 20:51:17 +0530

    added

--- NQT/Prime.java
+++ NQT/Prime.java
@@ -1,33 +1,72 @@
-class Prime {
-    static boolean isPrime(int n) {
+// class Prime {
+//     static boolean isPrime(int n) {
       
-        // Check if n is 1 or 0
-        if (n <= 1)
-            return false;
+//         // Check if n is 1 or 0
+//         if (n <= 1)
+//             return false;
 
-        // Check if n is 2 or 3
-        if (n == 2 || n == 3)
-            return true;
+//         // Check if n is 2 or 3
+//         if (n == 2 || n == 3)
+//             return true;
 
-        // Check whether n is divisible by 2 or 3
-        if (n % 2 == 0 || n % 3 == 0)
-            return false;
+//         // Check whether n is divisible by 2 or 3
+//         if (n % 2 == 0 || n % 3 == 0)
+//             return false;
         
-        // Check from 5 to square root of n
-        // Iterate i by (i+6)
-        for (int i = 5; i * i <= n; i = i + 6)
-            if (n % i == 0 || n % (i + 2) == 0)
-                return false;
+//         // Check from 5 to square root of n
+//         // Iterate i by (i+6)
+//         for (int i = 5; i * i <= n; i = i + 6)
+//             if (n % i == 0 || n % (i + 2) == 0)
+//                 return false;
 
-        return true;
+//         return true;
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 11; 
+//       	if(isPrime(n)){
+//         	System.out.println("true");
+//         }else{
+//           System.out.println("false");
+//     	}
+//     }
+// }
+
+
+import java.util.Scanner;
+
+
+public class Prime{
+
+    static boolean isPrime(int n){
+        // if(n <= 1)
+        //     return false;
+        if(n==2 || n==3)
+            return true;
+        if(n % 2 == 0 || n % 3 == 0)
+            return false;
+        for(int i=5; i*i <= n; i = i+6){
+            if(n % i == 0 || n % (i+2) == 0)
+                return false;
+        
+        }
+            return true;
+        
+
     }
 
-    public static void main(String[] args) {
-        int n = 11; 
-      	if(isPrime(n)){
-        	System.out.println("true");
-        }else{
-          System.out.println("false");
-    	}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range: ");
+        int n = sc.nextInt();
+
+        if(n < 2)
+            System.out.println("No prime num within this range.");
+    
+        for(int i=2; i <=n; i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
     }
 }
